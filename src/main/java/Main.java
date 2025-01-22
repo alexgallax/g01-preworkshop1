259a25e0e62f059c31b104a8e9ecfb9f3bd8a0ef
Вовк Алексей Анатольевич <avovk@samokat.ru>
2025-01-22 12:24:51 +0300

    add main

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import cargo.Car;
+import cargo.Notebook;
+import cargo.Pineapple;
+import cargo.Sand;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Состав груза:
+
+        Car van = new Car(4500);
+        Notebook notebooksContainer = new Notebook(100);
+        Pineapple pineapplesBox = new Pineapple(500);
+        Sand sandContainer = new Sand(3000);
+        Sand sandBag = new Sand(100);
+
+        // Реализуй подсчет общего веса груза:
+
+    }
+}
